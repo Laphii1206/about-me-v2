9862f438c0fd10693c132f1468ff45b60a8f2d4d
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-10-23 21:45:58 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,16 +35,16 @@ function App() {
           </div>
 
           <div className="flex flex-col max-w-md md:ml-8 text-center md:text-left">
-            <h1 className="text-3xl sm:text-4xl font-bold mb-4">My Wife!</h1>
+            <h1 className="sm:text-7xl font-bold mb-4">My Wife!</h1>
 
-            <p className="text-sm mb-4 font-semibold leading-relaxed">
+            <p className="text-lg mb-12 font-semibold leading-relaxed">
               天然でお日様のように明るく、周囲から好かれる生まれながらのお嬢様。
               飾らない人柄が男女ともに人気で、学園のアイドル的な存在になっている。
               特定の恋人を作る気はないようだが、
               お人好しで告白を断るのにいつも苦労しているらしい。
             </p>
 
-            <div className="text-sm mb-5 space-y-2">
+            <div className="text-lg mb-5 space-y-2">
               <p><strong>Measurements:</strong> Height: 150cm, Weight: 41kg</p>
               <p><strong>Affiliation:</strong> 香々見学園本校1年1組</p>
               <p><strong>Birthday:</strong> 8 December</p>
