37d303cb907c755d041689ef9dc64bb04976e5f8
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-12-18 05:02:16 +0800

    pe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import cover from '/cover.jpg'
 
 import Wife from './assets/sections/Wife.jsx'
 import Heading from './assets/sections/Heading.jsx'
@@ -8,18 +7,12 @@ import Footer from './assets/sections/Footer.jsx'
 
 function App() {
   return (
-    <div
-      className="min-h-screen bg-cover bg-center bg-fixed"
-      style={{ backgroundImage: `url(${cover})` }}
-    >
+    <>
       <Heading />
       <Wife />
       <OsuCommunity />
       <Footer />
-
-    </div>
-
-
+    </>
   )
 }
 
