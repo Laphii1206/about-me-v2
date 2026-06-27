59589d81e3a782ca16e1acdf9223b37aac450a2e
Laphii <91127087+Laphii1206@users.noreply.github.com>
2026-06-28 01:00:31 +0800

    fix my button

--- src/assets/Button/Button.jsx
+++ src/assets/Button/Button.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { SiOsu, SiX, SiSteam, SiGoogleSheets } from 'react-icons/si'
-import { FaTrophy, FaBook } from 'react-icons/fa'
+import { SiOsu, SiX, SiSteam, SiGooglesheets  } from 'react-icons/si'
+import { FaBook } from 'react-icons/fa'
 
 export const btnStyle = "inline-flex items-center justify-center px-4 py-2 bg-blue-600 ease-in-out delay-75 hover:bg-blue-700 text-white text-sm font-medium rounded-md active:scale-95 transition-all duration-200";
 
 const content = [
   { label: "osu!", url: "https://osu.ppy.sh/users/9364594", icon: <SiOsu /> },
   { label: "VNDB", url: "https://vndb.org/u228751/ulist?f=&mul=1&p=1&s=3q02", icon: <FaBook /> },
-  { label: "osu! Tournament History", url: "https://docs.google.com/spreadsheets/d/1dCUqfqQC7rHrAj6Tg9IQXhuwJd_hIWo1mj7pRJ0q1OE/edit#gid=1040639049", icon: <SiGooglesheets /> },
-  { label: "X", url: "https://x.com/nishikilaphii", icon: <SiX /> },
+  { label: "osu! Tournament History", url: "https://docs.google.com/spreadsheets/d/1dCUqfqQC7rHrAj6Tg9IQXhuwJd_hIWo1mj7pRJ0q1OE/edit#gid=1040639049", icon: <SiGooglesheets  /> },
+  { label: "Twitter", url: "https://x.com/nishikilaphii", icon: <SiX /> },
   { label: "Steam", url: "https://steamcommunity.com/id/laphii", icon: <SiSteam /> },
 ];
 
