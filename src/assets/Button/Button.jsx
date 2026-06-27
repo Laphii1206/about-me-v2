36b344761babf8d81706fa465638498748d9a318
Laphii <91127087+Laphii1206@users.noreply.github.com>
2026-06-28 01:14:09 +0800

    update icon again

--- src/assets/Button/Button.jsx
+++ src/assets/Button/Button.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { SiOsu, SiX, SiSteam, SiGooglesheets  } from 'react-icons/si'
 import { FaBook } from 'react-icons/fa'
 
-export const btnStyle = "inline-flex items-center justify-center px-4 py-2 bg-blue-600 ease-in-out delay-75 hover:bg-blue-700 text-white text-sm font-medium rounded-md active:scale-95 transition-all duration-200";
-
+export const btnStyle = "inline-flex items-center justify-center px-4 py-2 bg-white/15 backdrop-blur-md border border-white/20 text-white text-sm font-medium rounded-full active:scale-95 hover:bg-white/25 hover:shadow-lg hover:shadow-white/5 transition-all duration-200 ease-in-out";
 const content = [
   { label: "osu!", url: "https://osu.ppy.sh/users/9364594", icon: <SiOsu /> },
   { label: "VNDB", url: "https://vndb.org/u228751/ulist?f=&mul=1&p=1&s=3q02", icon: <FaBook /> },
